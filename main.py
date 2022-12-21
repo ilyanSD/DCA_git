9d20c644300378e7a06e5c99fd3fd159bab90173
Ilya Slyusarchuk <ilyaniesvb@gmail.com>
2022-12-21 20:03:11 +0100

    Creada funcion restar

--- main.py
+++ main.py
@@ -1,10 +1,29 @@
 def sumar(a, b):
     return a + b
 
+def restar(a, b):
+    return a - b
+
 if __name__ == "__main__":
     print("[INICIANDO] Aplicacion inicializada!")
     
-    a = int(input("Ingrese el primer numero: "))
-    b = int(input("Ingrese el segundo numero: "))
+    option = -1
     
-    print("El resultado de la suma es:", sumar(a, b))
+    while option != 3:
+        print("1) Sumar")
+        print("2) Restar")
+        print("3) Salir")
+        option = int(input("Ingrese una opcion: "))
+        
+        if option == 1:
+            a = int(input("Ingrese el primer numero: "))
+            b = int(input("Ingrese el segundo numero: "))
+            print("[RESULT] El resultado es:", sumar(a, b))
+        elif option == 2:
+            a = int(input("Ingrese el primer numero: "))
+            b = int(input("Ingrese el segundo numero: "))
+            print("[RESULT] El resultado es:", restar(a, b))
+        elif option == 3:
+            print("[FINISHING] Saliendo...")
+        else:
+            print("[ERROR] Opcion invalida!")
